22d7de4cfde55946e16e01db8edb838422b0d344
Habibtusher <tusherhrt@gmail.com>
2021-07-02 16:57:52 +0600

    first commit

---
+++ src/Components/Todo.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import img from '../images/todo.png';
+import './Todo.css'
+const Todo = () => {
+    const [inputData, setInputData] = useState('');
+    const [todo, setTodo] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/allTodo")
+            .then(res => res.json())
+            .then(data => setTodo(data))
+    }, [todo])
+
+    const addItem = () => {
+        if (!inputData) {
+            alert('plzz fill data');
+        }
+        else {
+            const allInputData = {
+                date: new Date(),
+                name: inputData
+            }
+            const url = `http://localhost:5000/addTodo`;
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(allInputData)
+            })
+            setInputData('');
+        }
+    }
+
+    const deleteItem = (id) => {
+        fetch(`http://localhost:5000/deleteTodo/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(result => {
+                
+            })
+    }
+    return (
+        <>
+            <div className="container">
+                <div>
+                    <figure>
+                        <img style={{ height: "150px" }} src={img} alt="todologo" />
+                        <figcaption className="pt-3">Add Your List Here ✌</figcaption>
+                    </figure>
+
+                    <div className="addItems ">
+                        <input style={{ width: "40%", fontSize: "25px" }} className="shadow pt-2 pb-2" type="text" placeholder="✍ Add Items..."
+                            value={inputData}
+                            onChange={(e) => setInputData(e.target.value)}
+                        />
+                        <i className="fa fa-plus add-btn" title="Add Item" onClick={addItem}></i>
+
+                    </div>
+
+                    <div className="mt-4 pb-5 mb-5" >
+
+                        {
+                            todo.map((elem) => {
+                                return (
+                                    <div className=" eachItem d-flex align-items-center justify-content-between p-4" key={elem._id}>
+                                        <h4>{elem.name}</h4>
+                                        <div className="todo-btn">
+                                            <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => deleteItem(elem._id)}></i>
+                                        </div>
+                                    </div>
+                                )
+                            })
+
+                        }
+
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Todo;
